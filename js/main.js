9283c4ca2981b2b845348f41493a3fb26cc3d96a
pascal-codecat <pascal.zwicker@windowslive.com>
2016-10-09 19:54:56 +0200

    added pattern und finishtouch navbar

--- js/main.js
+++ js/main.js
@@ -2,6 +2,23 @@
 $(document).ready(function(){
 
 
+$('#loading').animate({width: 200, marginLeft: 0}, {duration: 1000});
+
+$('.nav-list').mouseenter(function(){
+	$(this).animate({width: '+=3vw'},'fast')
+	.css('background-image','url(../img/45degreee.png)')
+	.css('font-weight','900')
+	.css('color','#008080');
+
+}).mouseleave(function(){
+	$(this).animate({width: '-=3vw'},'fast')
+	.css('background-image','url(../img/45degreeeLightblue.png)');
+	$(this).css('font-weight','normal')
+	.css('color','#e8e8e8');
+});
+
+
+
 // START SMOOTH SCROLLING
 
 $('.willkommen-l').click(function(){
